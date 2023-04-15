25f3f6f0b702e5469b669598d6d2a8d6059d3865
morveine <morveine@gmail.com>
2023-04-15 20:28:29 +0200

    add more styling, start writing logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# calculator
+Font: Digital-7 Regular (more info in assets/readme.txt)
+
+Buttons: super talented [Kathy](https://codepen.io/kathykato)
---
+++ script.js
@@ -0,0 +1,39 @@
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const clear = document.getElementById('clear');
+const del = document.getElementById('delete');
+const dot = document.getElementById('dot');
+const equals = document.getElementById('equals');
+const screen = document.getElementById('calc-screen');
+let temp = 0;
+
+clear.addEventListener('click', clearScreen);
+del.addEventListener('click', clearScreen);
+dot.addEventListener('click', inputHandler);
+equals.addEventListener('click', operationHandler);
+digits.forEach(digit => digit.addEventListener('click', inputHandler))
+digits.forEach(digit => digit.addEventListener('click', operationHandler))
+
+
+
+function clearScreen() {
+    if (this.id === 'clear') {
+        screen.textContent = '';
+    } else {
+        screen.textContent = screen.textContent.slice(0, -1);
+    }
+}
+
+
+function inputHandler(event) {
+    const buttonText = document.createTextNode(event.target.textContent); // gets the text content of the clicked button 
+    if (event.target !== dot || !screen.textContent.includes('.')) {
+        if (screen.textContent.length >= 11) {
+            screen.textContent = screen.textContent.slice(1);
+        };
+        screen.appendChild(buttonText);
+    }
+
+} // add input depending on which button was pressed, for 'dot' should be once
+
+function operationHandler() { }
